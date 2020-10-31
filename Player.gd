67b5bc46c66cbf0bbd0adbe13334e3a2f4b6d276
caribbean444 <Misha.bogatow@yandex.ru>
2020-10-31 19:41:20 +0300

    init
    init

---
+++ Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var speed = 50
+var jump_force = 100
+var gravity = 500
+
+var vel = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("player_left"):
+		vel.x -= speed
+	elif Input.is_action_pressed("player_right"):
+		vel.x += speed
+	move_and_slide(vel, Vector2.UP) 
